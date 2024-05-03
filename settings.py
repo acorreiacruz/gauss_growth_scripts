5a444efd5bbe63f5903696d5460a2902b58da43f
acorreiacruz <antoniocruzjunior@outlook.com>
2024-05-03 20:13:33 -0300

    feat: project settings.

---
+++ settings.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+VTEX_ACCOUNT_NAME = os.environ.get("VTEX_ACCOUNT_NAME")
+VTEX_ENVIRONMENT = os.environ.get("VTEX_ENVIRONMENT")
+VTEX_KEY = os.environ.get("VTEX_KEY")
+VTEX_TOKEN = os.environ.get("VTEX_TOKEN")
+VTEX_API_HOST = f"https://{VTEX_ACCOUNT_NAME}.{VTEX_ENVIRONMENT}.com.br/api"
