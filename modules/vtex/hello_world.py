c4602d4dfe92eb6c5fe22d98a22efb5c04fe26a2
acorreiacruz <antoniocruzjunior@outlook.com>
2024-05-04 08:23:48 -0300

    feat: test function.

---
+++ modules/vtex/hello_world.py
@@ -0,0 +1,8 @@
+from functions_framework import http
+from flask import Request
+from flask.typing import ResponseReturnValue
+
+@http
+def hello_world(request: Request) -> ResponseReturnValue:
+    if request.method == "GET":
+        return "Hello World !"
