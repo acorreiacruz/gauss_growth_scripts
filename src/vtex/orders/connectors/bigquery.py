8d85f8a3757813bcc3ac1720f6da201bde713abc
acorreiacruz <antoniocruzjunior@outlook.com>
2024-05-05 17:24:00 -0300

    refactor: BigQueryConnector.

--- src/vtex/orders/connectors/bigquery.py
+++ src/vtex/orders/connectors/bigquery.py
@@ -5,5 +5,6 @@ class BigQueryConnector:
     def __init__(self) -> None:
         self.client = Client()
 
-    def insert_rows(self, table: str, data: list) -> None:
-        self.client.insert_rows_json(table=table, json_rows=data)
+    def insert_rows(self, table: str, data: list, **kwargs) -> list:
+        results = self.client.insert_rows_json(table=table, json_rows=data, **kwargs)
+        return results
